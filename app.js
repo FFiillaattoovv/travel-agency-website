b27df19969eb2eb1516f97930ce6187ff34e09ad
FFiillaattoovv <admirator2015@yandex.ru>
2021-12-24 10:55:11 +0300

    Migration to express

--- app.js
+++ app.js
@@ -1,36 +1,30 @@
-const http = require('http');
-const fs = require('fs');
+const express = require('express');
+const app = express();
+
 const port = process.env.PORT || 3000;
 
-const serveStaticFile = (res, path, contentType, responseCode = 200) => {
-    fs.readFile(__dirname + path, (err, data) => {
-        if (err) {
-            res.writeHead(500, {'Content-Type': 'text/plain'});
-            return res.end('500 - Internal Error');
-        }
-        res.writeHead(responseCode, {'Content-Type': contentType});
-        res.end(data);
-    });
-};
-
-const server = http.createServer((req, res) => {
-    const path = req.url.replace(/\/?(?:\?.*)?$/, '').toLowerCase();
-
-    switch (path) {
-        case '':
-            serveStaticFile(res, '/public/home.html', 'text/html');
-            break;
-        case '/about':
-            serveStaticFile(res, '/public/about.html', 'text/html');
-            break;
-        case '/img/logo.png':
-            serveStaticFile(res, '/public/img/logo.png', 'image/png');
-            break;
-        default:
-            serveStaticFile(res, '/public/404.html', 'text/html', 404);
-            break;
-    }
+app.get('/', (req, res) => {
+    res.type('text/plain');
+    res.send('Home page');
 });
 
-server.listen(port, () => console.log(`server started on port ${port}; ` +
-    'press Ctrl-C to terminate....'));
+app.get('/about', (req, res) => {
+    res.type('text/plain');
+    res.send('About page');
+});
+
+app.use((req, res) => {
+    res.type('text/plain');
+    res.status(404);
+    res.send('404 - Not Found');
+});
+
+app.use((req, res, next, err) => {
+    console.error(err.message);
+    res.type('text/plain');
+    res.status(500);
+    res.send('500 - Server Error');
+});
+
+app.listen(port, () => console.log(`Express started on http://localhost:${port}; ` +
+    `press Ctrl-C to terminate.`));
