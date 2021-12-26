43036f04669f67a811f7201277a5a59d3f3466c7
FFiillaattoovv <admirator2015@yandex.ru>
2021-12-26 15:46:30 +0300

    Add module tests

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const expressHandlebars = require('express-handlebars');
-const getFortune = require("./lib/fortune");
+const handlers = require("./lib/handlers");
 
 const app = express();
 
@@ -13,23 +13,12 @@ app.use(express.static(__dirname + '/public'));
 
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => res.render('home'));
+app.get('/', handlers.home);
 
-app.get('/about', (req, res) => {
-    res.render('about', {fortune: getFortune()});
-});
+app.get('/about', handlers.about);
 
-app.use((req, res) => {
-    res.status(404);
-    res.render('404');
-});
+app.use(handlers.notFound);
 
-app.use((req, res, next, err) => {
-    console.error(err.message);
-    res.status(500);
-    res.render('500');
-});
+app.use(handlers.serverError);
 
-app.listen(port, () => console.log(
-    `Express started on http://localhost:${port}; ` +
-    `press Ctrl-C to terminate.`));
+app.listen(port, () => console.log(`Express started on http://localhost:${port}; ` + `press Ctrl-C to terminate.`));
